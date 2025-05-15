b2fa7a683d2b2193868866dea50db6ae75e357a3
ElizavetaShin <Elis.Shin.G@gmail.com>
2025-05-15 18:56:20 +0300

    cursor

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,9 @@
 let activeFigure = document.getElementById ("active-figure");
+let cursorCircle = document.getElementById ("cursor-circle");
 let activeFigureX = 0;
 let activeFigureY = 0;
+let cursorCircleX = 0;
+let cursorCircleY = 0;
 let clientX = 0;
 let clientY = 0;
 window.addEventListener('mousemove',(event)=>{
@@ -16,5 +19,15 @@ function updateActiveFigure (){
          updateActiveFigure ()
     })
 }
-
+function updateCursorCircle (){
+    cursorCircleX  += (clientX - cursorCircleX )/30;
+    cursorCircleY  += (clientY - cursorCircleY)/30;
+    cursorCircle.style.transform = `translate(${Math.round(cursorCircleX)}px,${Math.round(cursorCircleY)}px)`;
+    requestAnimationFrame (()=>{
+         updateCursorCircle ()
+    })
+}
 updateActiveFigure ();
+updateCursorCircle ();
+
+
