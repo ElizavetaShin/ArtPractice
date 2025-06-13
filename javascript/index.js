9ba2b51c2087a926604bea3e1cd63c0875b8755e
ElizavetaShin <Elis.Shin.G@gmail.com>
2025-06-13 19:45:55 +0300

    SignsAnimation

--- javascript/index.js
+++ javascript/index.js
@@ -31,3 +31,25 @@ updateActiveFigure ();
 updateCursorCircle ();
 
 
+
+
+
+
+
+function animateSignRotating (signElement, x0, y0, r, speed, isForward){
+    let prevTime = performance.now();
+    signElement.style.left = x0;
+    signElement.style.top = y0;
+
+
+    requestAnimationFrame ((time)=>{
+       let timeSincePrev = time-prevTime;
+       let deg = timeSincePrev*speed*Math.PI/180;
+       let xValue = +signElement.style.translate.split('px')[0].trim();
+       let yValue = +signElement.style.translate.split('px')[1].trim();   
+       let x = x0 + r*Math.cos(deg);
+       let y = y0 + r*Math.sin(deg);
+       signElement.style.translate = `${x-x0}px ${y-y0}px`
+       prevTime = time;
+    })
+}
