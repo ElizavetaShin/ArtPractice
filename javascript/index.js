589b4e5a1f928017973b8a0c015810bf285f55b0
ElizavetaShin <Elis.Shin.G@gmail.com>
2025-06-24 19:39:46 +0300

    ad

--- javascript/index.js
+++ javascript/index.js
@@ -34,14 +34,13 @@ updateCursorCircle ();
 
 
 
-
-
+let signsZone = document.getElementById('signs-zone');
 function animateSignRotating (signElement, x0, y0, r, speed, startDeg, isForward){
     let prevTime = performance.now();
     let deg = startDeg;
     let coef = isForward ? 1 : -1;
-    signElement.style.left = x0+'px';
-    signElement.style.top = y0+'px';
+    signElement.style.left = x0 - signElement.clientWidth/2 +'px';
+    signElement.style.top = y0 - signElement.clientHeight/2 +'px';
     speed = speed/1000;
 
     requestAnimationFrame (function rotating(time){
@@ -49,21 +48,45 @@ function animateSignRotating (signElement, x0, y0, r, speed, startDeg, isForward
        let timeSincePrev = time-prevTime;
        deg = (coef*timeSincePrev*speed + deg) % 360;
        let radians = deg*Math.PI/180;
-       let x = x0 + r*Math.cos(radians);
-       let y = y0 + r*Math.sin(radians);
-       signElement.style.translate = `${x-x0}px ${y-y0}px`
+       let x = r*Math.cos(radians);
+       let y = r*Math.sin(radians);
+       signElement.style.translate = `${x}px ${y}px`
+       
        prevTime = time;
        requestAnimationFrame (rotating);
     })
 }
 
-
-let signsZone = document.getElementById('signs-zone');
+function animateSignsCircle(x0, y0, r, speed, isForward){
+    let prevTime = performance.now();
+    let deg = 0;
+    let coef = isForward ? 1 : -1;
+    speed = speed/1000;
+    let circle = document.createElement('div');
+    circle.classList.add('signs-circle');
+    circle.style.height = r*2 + "px";
+    circle.style.width = r*2 + "px";
+    circle.style.left = x0 - r + 'px';
+    circle.style.top = y0 - r + 'px';
+    signsZone.append(circle);
+    requestAnimationFrame(function circleRotating(time){
+        let timeSincePrev = time-prevTime;
+        deg = (coef*timeSincePrev*speed + deg) % 360;   
+        circle.style.rotate = `${deg}deg`;
+        prevTime = time; 
+        requestAnimationFrame(circleRotating);
+    })
+}
 let signsArr = [];
 let reverseSignsArr = [];
 let signsAmount = 17;
 let reverseSignsAmount = 14;
-
+let circleCenterX = window.innerWidth*1.12;
+let circleCenterY = window.innerWidth*0.34;
+let bigCircleRadius = window.innerWidth*0.4;
+let smallCircleRadius = window.innerWidth*0.32;
+let bigCircleSpeed = 10;  //градусы в сек
+let smallCircleSpeed = 8;
 for(let i = 0; i<signsAmount; i++){
     let sign = document.createElement('div');
     sign.classList.add('sign');
@@ -81,11 +104,13 @@ for(let i = 0; i<reverseSignsAmount; i++){
     signsZone.append(sign);
 }
 
-
+animateSignsCircle(circleCenterX, circleCenterY, bigCircleRadius, bigCircleSpeed, true);
 signsArr.forEach((element, index)=>{
-    animateSignRotating(element, window.innerWidth*1.12, window.innerWidth*0.34, window.innerWidth*0.4, 10, index*360/signsAmount, true);
+    animateSignRotating(element, circleCenterX, circleCenterY, bigCircleRadius, bigCircleSpeed, index*360/signsAmount, true);
 })
 
+
+animateSignsCircle(circleCenterX, circleCenterY, smallCircleRadius, smallCircleSpeed, false);
 reverseSignsArr.forEach((element, index)=>{
-    animateSignRotating(element, window.innerWidth*1.12, window.innerWidth*0.34, window.innerWidth*0.32, 8, index*360/reverseSignsAmount, false);
+    animateSignRotating(element, circleCenterX, circleCenterY, smallCircleRadius, smallCircleSpeed, index*360/reverseSignsAmount, false);
 })
